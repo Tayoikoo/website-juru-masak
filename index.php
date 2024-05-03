2faf2805cd8a2d19b83d79eea510e0f1e44470bb
piimon <luthfifalseman@gmail.com>
2024-05-03 10:49:13 +0700

    aplot fail php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Komunitas Memasak</title>
+</head>
+<body>
+    <h1>Project Website Memasak</h1>
+    <p>Komunitas Memasak merupakan komunitas yang aktif dalam berbagi resep memasak, pengalaman memasak</p>
+    <p>Untuk Saat ini masih dalam tahap awal, jika ingin berkontribusi silahkan ke link ini</p>
+    <a href="https://github.com/Tayoikoo/website-juru-masak">
+        <button>Link</button>
+    </a>
+</body>
+</html>
